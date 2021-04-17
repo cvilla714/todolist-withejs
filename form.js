773daf2cd76eab5a63c0d525017c2232ddb081c8
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-17 19:28:11 -0400

    added the new variable at the top outside the post request and get request to have access to it

--- form.js
+++ form.js
@@ -4,6 +4,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
+let newitem = '';
+
 app.get('/', (req, res) => {
   const today = new Date();
 
@@ -14,12 +16,16 @@ app.get('/', (req, res) => {
   };
 
   let day = today.toLocaleDateString('en-US', options);
-  res.render('form', { kindOfDay: day });
+  res.render('form', { kindOfDay: day, newlistitem: newitem });
 });
 
 app.post('/', (req, res) => {
   console.log(req.body);
   console.log(req.body.wakanda);
+  //   let newitem = req.body.wakanda;
+  //   res.render('form', { newlistitem: newitem });
+  newitem = req.body.wakanda;
+  res.redirect('/');
 });
 
 app.listen(port, () => {
