11f6d16a572d82e2d1882cfce16759b7a629b3e1
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-18 01:16:47 -0400

    added express to use static content inside the public folder

--- form.js
+++ form.js
@@ -3,6 +3,7 @@ const port = 5000;
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // let newitem = '';
 let newtaks = ['Buy Groceries', 'Buy Milk', 'Buy IceCream'];
